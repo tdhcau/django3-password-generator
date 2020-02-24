822a7f8da8309b82cff4571c23cff19560832a7e
C T <ct@example.com>
2020-02-24 19:50:07 +1100

    First commit

---
+++ generator/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+# Create your views here.
+
+def home(request):
+    #return HttpResponse('Hello World! Welcome to my page')
+    return render(request, 'generator/home.html')
+
+def password(request):
+    #thepassword = 'testing'
+
+    characters = list('abcdefghijklmnopqrstuvxyz')
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*'))
+    length = int(request.GET.get('length', 8))
+    thepassword =''
+    for i in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': thepassword})
+
+def about(request):
+    #return HttpResponse('Hello World! Welcome to my page')
+    return render(request, 'generator/about.html')
